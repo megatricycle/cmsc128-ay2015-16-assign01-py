65dbb3b9a0d869e8ccf3c992f0ef77d351280e72
Peter Rupa <megatricycle@gmail.com>
2016-02-14 08:58:54 +0800

    initial commit

---
+++ numLib.py
@@ -0,0 +1,16 @@
+##############################################
+# numLib, a library for trivial number functions
+# Peter Bernard M. Rupa
+##############################################
+
+def numToWords(input):
+    return
+    
+def wordsToNum(input):
+    return
+    
+def wordsToCurrency(input, prefix):
+    return
+    
+def numberDelimited(input, delimiter, offset):
+    return
---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+import numLib
+
+class NumLibTesting(unittest.TestCase):
+    def test_numToWords(self):
+        self.assertEqual(1, 1);
+          
+if __name__ == '__main__':
+    unittest.main()
